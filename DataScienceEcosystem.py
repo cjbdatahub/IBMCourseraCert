4a32032d93670aee8e1afba8ee0bbbc949478988
CJBDataHub <79811061+cjbdatahub@users.noreply.github.com>
2023-08-08 21:31:13 -0400

    Python Notebook - Assignment
    Final Assignment Instructions: Create and Share Your Jupyter Notebook

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## Author
+# Chris
+
+# This notebook provides a summary of Data Science Tools and EcoSystem.
+
+# __ Objectives: __
+# * List Popular Languages for Data Science
+# * Learn how to naivagate and notetate JupyterNotebook
+# * Engage in basic Python functionality
+# * Libraries Popular with Data Scientists
+# 
+# 
+
+# There are several languages that are quite popular amongst Data Scientists such as:
+
+# 1. Python
+# 2. R
+# 3. SQL
+
+# The most commonly used libraries utilized by Data Scientists include:
+
+# ---
+
+# 1. ggplot2 
+# 2. PyTorch
+# 3. dplyr
+
+# | Data Science Tools |
+# -------------------------------
+# | R STUDIO |
+# | JUPYTERLAB |
+# | PYCHARM COMMUNITY EDITION |
+# 
+
+# ### Please see below for several examples of evaluating arithmetic expressions in Python
+
+# This is a rather rudimentary example of an arithmetic expression, muliplying then adding integers.
+
+# In[3]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by dividing by 60
+
+# In[6]:
+
+
+min = 200
+hr = min / 60
+hr
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
